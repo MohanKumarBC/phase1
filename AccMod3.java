047fb5eabbf1aab0abdfb5dc6d8da1274a77653e
Mohan Kumar B C <100827383+MohanKumarBC@users.noreply.github.com>
2022-03-06 23:54:35 +0530

    Add files via upload

---
+++ AccMod3.java
@@ -0,0 +1,11 @@
+package FortyPgms2;
+import FortyPgms.*;
+public class AccMod3 extends ProtectedMod {
+
+		public static void main(String[] args) {
+			AccMod3 obj = new AccMod3();   
+		       obj.display();  
+		}
+
+
+}
---
+++ AccMod4.java
@@ -0,0 +1,13 @@
+package FortyPgms2;
+
+import FortyPgms.PublicMod;
+
+public class AccMod4 {
+
+	public static void main(String[] args) {
+		PublicMod obj=new PublicMod();
+		obj.display();
+		
+	}
+
+}
